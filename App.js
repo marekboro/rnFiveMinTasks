acba9a85184b63df78335c36a1d5ddac0b2b9658
marekboro <marek.borowicz83@gmail.com>
2020-12-12 13:21:44 +0000

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+
+const App = () => {  // This will be our component. 
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+  
+};
+
+export default App;
